41e7310f171bbcef9b1f3f2ef4b15a7f740506ab
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-08-03 10:03:53 +0100

    new layout using tab

--- src/layouts/base.py
+++ src/layouts/base.py
@@ -5,19 +5,14 @@ kivy.require("2.2.1")
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button, ButtonBehavior
-from kivy.properties import ListProperty
+
+from src.layouts.tabs import TabLayout
 
 Builder.load_file("src/dl/base.kv")
 
 
 class CustomButton(Button, ButtonBehavior):
-    background = ListProperty((3 / 255, 35 / 255, 61 / 255, 1))
-
-    def on_enter(self):
-        self.background = (134 / 255, 187 / 255, 240 / 255, 1)
-
-    def on_leave(self):
-        self.background = (3 / 255, 35 / 255, 61 / 255, 1)
+    pass
 
 
 class BaseWidget(Widget):
---
+++ src/layouts/tabs.py
@@ -0,0 +1,8 @@
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.lang import Builder
+
+Builder.load_file("src/dl/tab.kv")
+
+
+class TabLayout(TabbedPanel):
+    pass
