b62c9ccd4602048d15ff5cb9a0e47faf3f9d931a
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-08-18 21:19:00 +0100

    remove mock data and other comments

--- src/layouts/homeList.py
+++ src/layouts/homeList.py
@@ -9,25 +9,15 @@ from kivy.properties import BooleanProperty, StringProperty
 
 from src.db.models.remindme import RemindMe
 
-# from kivy.base import runTouchApp
-
 
 class RemainderListView(RecycleView):
-    mock_data = [
-        ("DCI Short Break 1", "10:30:00"),
-        ("DCI Lunch Break", "12:15:00"),
-        ("DCI Short Break 2", "13:40:00"),
-    ]
-
     def __init__(self, **kwargs):
         super(RemainderListView, self).__init__(**kwargs)
         self.data = self.prepare_data()
-        # self.data = [{"text": l, "other_text": t} for l, t in self.mock_data]
 
     def prepare_data(self):
         all_data = []
         reminders: List[RemindMe] = RemindMe.get_all()
-        # Reminder = namedtuple("Reminder", "id label days alert_time alert_type active")
         for reminder in reminders:
             result = {
                 "text": reminder.label,
@@ -41,12 +31,6 @@ class RemainderListView(RecycleView):
             }
             all_data.append(result)
         return all_data
-
-    # (11, 'first', ['Fri', 'Sat'], datetime.time(19, 27), {'popup': True, 'sound': False}, True)
-
-
-# [(11, 'first', ['Fri', 'Sat'], datetime.time(19, 27), {'popup': True, 'sound': False}, True),
-#  (12, 'second', ['Fri'], datetime.time(19, 30), {'popup': True, 'sound': False}, True)]
 
 
 class ReminderState(RecycleDataViewBehavior, BoxLayout):
