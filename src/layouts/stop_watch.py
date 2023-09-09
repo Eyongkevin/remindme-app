14beda14209be9aeefb0029a0eb6c8a14574c5f4
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-09-09 23:55:18 +0100

    apply stoptwatch's start, pause, and reset buttons

--- src/layouts/stop_watch.py
+++ src/layouts/stop_watch.py
@@ -8,8 +8,12 @@ from kivy.properties import ObjectProperty
 
 class SetStopWatchPopup(Popup):
     def dismiss(self, *_args, **kwargs):
-        _args[0].sw_started = True
+        _args[0].sw_state = "start"
         return super().dismiss(*_args, **kwargs)
+
+
+class RequestStartPopup(Popup):
+    pass
 
 
 class StopWatchBoxLayout(BoxLayout):
@@ -20,8 +24,19 @@ class StopWatchBoxLayout(BoxLayout):
         StopWatchBoxLayout.label_ins = inst
         return ""
 
-    def pause(self, app):
-        app.sw_started = False
+    def pause(self, ins, app):
+        if app.sw_state in ["start", "pause"] and app.sw_stop_time is not False:
+            if ins.text == "Resume":
+                app.sw_state = "start"
+                ins.text = "Pause"
+            else:
+                app.sw_state = "pause"
+                ins.text = "Resume"
+        else:
+            RequestStartPopup().open()
+
+    def reset(self, app):
+        app.sw_state = "reset"
 
 
 class StopWatchLabel(Label):
@@ -33,6 +48,15 @@ class StopWatchLabel(Label):
 
 
 class StopWatchInputText(TextInput):
+    def __init__(self, **kwargs):
+        self.root_app = None
+        super().__init__(**kwargs)
+
     def on_text(self, instance, value):
         if StopWatchLabel.label_ins is not None:
             StopWatchLabel.label_ins.text = value + " mins"
+            if self.root_app is not None:
+                try:
+                    self.root_app.sw_stop_time = int(value)
+                except ValueError:
+                    self.root_app.sw_stop_time = False
--- src/main.py
+++ src/main.py
@@ -9,22 +9,38 @@ from src.scheduler import Scheduler
 
 
 class MainApp(App):
-    sw_started = False
+    sw_state = False
+    sw_time = 0
+    sw_stop_time = False
 
     def update_time(self, nap):
-        if self.sw_started:
-            self.sw_started += nap
-        minutes, seconds = divmod(self.sw_started, 60)
-        part_seconds = seconds * 100 % 100
-        self.root.children[0].children[
-            0
-        ].ids.stopwatch.text = f"""{int(minutes):02}:{int(seconds):02}.[size=40]{int(part_seconds):02}[/size]"""
-
+        if self.sw_state == "start" and self.sw_stop_time:
+            self.sw_time += nap
+            minutes, seconds = divmod(self.sw_time, 60)
+            if int(minutes) >= self.sw_stop_time:
+                MainApp.sw_state == "stop"
+            else:
+                part_seconds = seconds * 100 % 100
+                self.root.children[0].children[
+                    0
+                ].ids.stopwatch.text = f"""{int(minutes):02}:{int(seconds):02}.[size=40]{int(part_seconds):02}[/size]"""
+        elif self.sw_state == "pause":
+            pass
+        elif self.sw_state == "reset":
+            self.root.children[0].children[
+                0
+            ].ids.stopwatch.text = "00:00.[size=40]00[/size]"
+            self.sw_time = 0
+            self.sw_stop_time = False
+            self.sw_state = False
+        elif self.sw_state == "stop":
+            self.sw_state = False
+        elif self.sw_state == "start" and self.sw_stop_time is False:
+            self.sw_state = False
         self.root.children[0].children[0].ids.time.text = strftime("[b]%H[/b]:%M:%S")
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
-        # self.update_time()
 
     def build(self):
         Window.size = (300, 500)
