79ee75f8cafc48da65a3e46f5156a8639b6d6abb
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-09-30 18:39:48 +0100

    many changes

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from time import strftime
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.clock import Clock
+from kivy.utils import get_color_from_hex
 
 import schedule
 from src.layouts import base
 from src.scheduler import Scheduler
+from .utils import load_fonts, seconds_until_midnight
+from src.layouts.homeList import RemainderListView
 
 
 class MainApp(App):
@@ -14,6 +17,14 @@ class MainApp(App):
     sw_stop_time = False
 
     def update_time(self, nap):
+        self._update_main_clock()
+        self._update_date()
+        self._update_stop_watch(nap)
+
+    def _update_main_clock(self):
+        self.root.children[0].children[0].ids.time.text = strftime("[b]%H[/b]:%M:%S")
+
+    def _update_stop_watch(self, nap):
         if self.sw_state == "start" and self.sw_stop_time:
             self.sw_time += nap
             minutes, seconds = divmod(self.sw_time, 60)
@@ -37,10 +48,21 @@ class MainApp(App):
             self.sw_state = False
         elif self.sw_state == "start" and self.sw_stop_time is False:
             self.sw_state = False
-        self.root.children[0].children[0].ids.time.text = strftime("[b]%H[/b]:%M:%S")
+
+    def _update_date(self):
+        self.root.children[0].children[0].ids.date.text = strftime("%a, %B %-d")
+
+    def _update_home_list_data(self, dt):
+        Scheduler.data_instance.data = RemainderListView.prepare_data()
+        self._schedule_once_at_midnight()
+
+    def _schedule_once_at_midnight(self):
+        secs = seconds_until_midnight()
+        Clock.schedule_once(self._update_home_list_data, secs)
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
+        self._schedule_once_at_midnight()
 
     def build(self):
         Window.size = (300, 500)
@@ -54,4 +76,6 @@ class MainApp(App):
 
 
 if __name__ == "__main__":
+    Window.clearcolor = get_color_from_hex("#101216")
+    load_fonts()
     MainApp().run()
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from datetime import datetime
+import datetime
+from kivy.core.text import LabelBase
 import anosql  # this seems to only work with pycopg2.
 import psycopg2
 
@@ -31,5 +32,22 @@ def build_dict(id: int, label: str, alert_time: datetime, active: bool):
         "text": label,
         "alert_time": str(alert_time),
         "active_img": "images/active.png" if active else "images/inactive.png",
-        "state": "Passed" if datetime.now().time() > alert_time else "Pending",
+        "state": "[color=#f74728]Passed[/color]"
+        if datetime.datetime.now().time() > alert_time
+        else "Pending",
     }
+
+
+def load_fonts():
+    font_path = ROOT_DIR / "static" / "fonts" / "Roboto"
+    LabelBase.register(
+        name="Roboto",
+        fn_regular=str(font_path / "Roboto-Thin.ttf"),
+        fn_bold=str(font_path / "Roboto-Medium.ttf"),
+    )
+
+
+def seconds_until_midnight():
+    now = datetime.datetime.now()
+    tomorrow = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+    return abs(tomorrow - now).seconds
