3738ec299f206beae4a5d5cb2e1ebeb17ee5aa29
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-10-26 12:51:21 +0100

    remove unused imports

--- src/layouts/home.py
+++ src/layouts/home.py
@@ -1,10 +1,6 @@
 from typing import List, Optional
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-from kivy.uix.popup import Popup
 from src.db.models.remindme import RemindMe
-from kivy.properties import ObjectProperty
 
 
 class UpcominigBoxLayout(BoxLayout):
